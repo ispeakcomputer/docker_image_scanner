3899179f283b93cf26b35b9251887470fcbb3d34
Ryan <ryan@ispeakcomputer.com>
2021-04-27 22:36:19 -0600

    packages up the dockerfiles and images nicely. Now to structure output per requirements

--- test.py
+++ test.py
@@ -81,12 +81,12 @@ mytoken = str(os.environ['GITHUBTOKEN'])
  
  
 class Dockerchecker:
+    #-----------------------------------Grab Text File------------------------
     def grab_txt_file(self, url):
         r = requests.get(url)
         mytext = r.text
         return mytext
- 
- 
+    #-----------------------------------Split And Package Text----------------
     def clean_and_package(self, mytext ):
         list_of_repos=[]
         for line in mytext.split('\n'):
@@ -94,24 +94,24 @@ class Dockerchecker:
             text  = line
             splittext = text.split(' ')
             
-            try:                                                              
+            try: #check for 'dead' line and add add sha > dict                                                          
                 splittext[1]                       
                 repo_info['sha'] = splittext[1]
             except IndexError:                   
                 pass  
-            #repo_info['sha'] = splittext[1]
             repo_info['url'] = splittext[0]
-            # Strip github url and file extension from 0 index
+
+            # get team/repo from url for searching w/ PyGithub
             parsed_string = splittext[0].replace('https://github.com/','')
             repo_info['user_repo'] = parsed_string.replace('.git','')
-            # remove dead line
-            if splittext[0] == '':
+           
+            if splittext[0] == '': #remove dead line
                 pass
             else:
                 list_of_repos.append(repo_info)
  
         return list_of_repos
-    
+    #-----------------------------------Combine SHA & URL---------------------
     def url_sha_combiner(self, dict_of_repos):
             for url in dict_of_repos:
                 combined = ":".join([url['url'],url['sha']])
@@ -121,30 +121,40 @@ class Dockerchecker:
     # ------------------------------------GITHUB------------------------------
     def parse_docker(self, mytoken, list_of_repos):
         g = Github(mytoken)
- 
-        for dic in list_of_repos:
+
+        for dic in list_of_repos:#for each repo
+
             repo = dic['user_repo']
+            dic['file_w_image']=[]
             repo_data = g.get_repo(repo)
             contents = repo_data.get_contents("")
             dfp = DockerfileParser()
- 
-            while contents:
+
+            while contents: #loop over file / path
                 file_content = contents.pop(0)
                 if file_content.type == "dir":
-                    contents.extend(repo_data.get_contents(file_content.path))
+                    contents.extend(repo_data.get_contents(file_content.path)) #add dir contents to search                                                                                                        
                 else:
-                    if file_content.name == 'Dockerfile':
+                    if file_content.name == 'Dockerfile':# each file get image list in dict
+                        file_images={}
+                        file_images['images']=[]
+                        file_images['Dockerfile'] = file_content.path
                         dfp.content = str(file_content.decoded_content, 'utf-8')
-                        container_repo = json.loads(dfp.json)
- 
-                        for dic in container_repo:
-                            if 'FROM' in dic:
+                        dockerfile_dicts = json.loads(dfp.json)
+
+                        for line_dict in dockerfile_dicts:
+                            if 'FROM' in line_dict:
                                 #Cleaning string. need only 1st col
-                                string_list = dic['FROM'].split(' ')
-                                print(string_list[0])
+                                string_list = line_dict['FROM'].split(' ')
+                                #print("string: " + string_list[0])
+                                file_images['images'].append(string_list[0])
+                                print(file_images)
+                        dic['file_w_image'].append(file_images)
+        return list_of_repos
 
 if __name__ == "__main__":
     checker = Dockerchecker()
+    data = {}
  
     text = checker.grab_txt_file(source)
     dict_of_repos_data = checker.clean_and_package(text)
