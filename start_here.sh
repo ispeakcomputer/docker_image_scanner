5f2281e4f1d14148732ba88289795d4493b71cad
Ryan <ryan@ispeakcomputer.com>
2021-05-17 19:05:42 -0600

    added start_here.sh for starting locally

---
+++ start_here.sh
@@ -0,0 +1,5 @@
+export GITHUBTOKEN=
+export REPOSITORY_LIST_URL=https://gist.githubusercontent.com/jmelis/c60e61a893248244dc4fa12b946585c4/raw/25d39f67f2405330a6314cad64fac423a171162c/sources.txt
+
+source venv/bin/activate
+python3 test.py
